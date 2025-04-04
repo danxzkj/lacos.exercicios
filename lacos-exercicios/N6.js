d7f4471da149ac9c2bc069f1d52444177b44484f
Daniel Casalli <danis.settk@gmail.com>
2025-04-04 11:44:50 -0300

    ex9

--- lacos-exercicios/N6.js
+++ lacos-exercicios/N6.js
@@ -1,4 +1,5 @@
-for (let i =1; i <=6; i++){
-    if(6!= 6*5*4*3*2*1 + i)
-        console.log(i)
+let fatorial = 6;
+let resultado = fatorial;
+for (let i =1; i < fatorial; i++){
+        console.log(resultado *= i)
 }
